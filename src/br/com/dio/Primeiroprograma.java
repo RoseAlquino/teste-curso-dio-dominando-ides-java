5b6170f8aec1ad0bba6dea11bfdc8b40c9759353
RoseAlquino <rosealqs@gmail.com>
2022-05-03 09:39:13 -0300

    Primeiro Commit

---
+++ src/br/com/dio/Primeiroprograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class Primeiroprograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* System.out.println("Hello World");*/
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+	}
+
+}
